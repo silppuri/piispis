d4e51f9a62706fd5ca84094edc61d4f43af58922
Petri Avikainen <petri@swarmia.com>
2025-08-13 13:36:25 +0300

    Update rust and modules

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,6 @@ module.exports = {
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, "."),
     }),
-    // Have this example work in Edge which doesn't ship `TextEncoder` or
-    // `TextDecoder` at this time.
-    new webpack.ProvidePlugin({
-      TextDecoder: ["text-encoding", "TextDecoder"],
-      TextEncoder: ["text-encoding", "TextEncoder"],
-    }),
     new CopyPlugin({
       patterns: [{ from: "static" }],
     }),
@@ -30,5 +24,8 @@ module.exports = {
   mode: "development",
   devServer: {
     host: "0.0.0.0",
+  },
+  experiments: {
+    asyncWebAssembly: true,
   },
 };
