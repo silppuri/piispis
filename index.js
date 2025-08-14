d12485b4370092edf723930c6d841dbf56886c60
Petri Avikainen <petri@swarmia.com>
2025-08-14 13:32:17 +0300

    Debug

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg';`
 // will work here one day as well!
-const rust = import("./pkg");
 
-rust.then((m) => m.main()).catch(console.error);
+import("./pkg")
+  .then((m) => {
+    console.log("Available exports:", Object.keys(m.default));
+    console.log("main function:", typeof m.default);
+    m.default.main();
+  })
+  .catch(console.error);
