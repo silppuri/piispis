66689ad2be8e98e9b3685e6fca81d4bb0686335f
Petri Avikainen <petri@swarmia.com>
2025-08-14 13:54:32 +0300

    Update wasm-pack

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
-// will work here one day as well!
+import { main } from "./pkg";
 
-import("./pkg")
-  .then((m) => {
-    console.log("Available exports:", Object.keys(m.default));
-    console.log("main function:", typeof m.default);
-    m.default.main();
-  })
-  .catch(console.error);
+main();
